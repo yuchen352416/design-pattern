901a6840e41da8f1bf0df8eec2806cddd7e7fbb8
Selfimpr <Selfimpr@SmiledeMacBook-Pro.local>
2018-12-09 18:16:08 +0800

    add new test...

--- src/test/java/pro/yuchen/chapter_001/Test.java
+++ src/test/java/pro/yuchen/chapter_001/Test.java
@@ -21,4 +21,39 @@ public class Test {
         duck.performFly();
     }
 
+    @org.junit.Test
+    public void test_003() { // 甜甜购--面试题
+        String str = " asdfjkltatyuiuytdfghj";
+        String[] arr = str.split("");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                if (arr[j].toCharArray()[0] > arr[j+1].toCharArray()[0]) {
+                    String t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    @org.junit.Test
+    public void test_004() { // 甜甜购--面试题
+        for (int i = 10000; i <= 99999; i++) {
+            int w1 = i / 10000;
+            int w2 = i / 1000 % 10;
+            int w3 = i / 100 % 10;
+            int w4 = i / 10 % 10;
+            int w5 = i % 10;
+
+            if ((w1 == w2 && w3 == w4) || (w2 == w3 && w4 == w5)) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
